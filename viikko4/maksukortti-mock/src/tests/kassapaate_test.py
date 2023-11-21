475e0b28f752ae982b917cafc1ca4e8e67c08df9
Henni Seppänen <104835482+henniseppis@users.noreply.github.com>
2023-11-21 17:32:16 +0200

    tehtävä2

--- viikko4/maksukortti-mock/src/tests/kassapaate_test.py
+++ viikko4/maksukortti-mock/src/tests/kassapaate_test.py
@@ -23,3 +23,19 @@ class TestKassapaate(unittest.TestCase):
         self.kassa.osta_lounas(maksukortti_mock)
 
         maksukortti_mock.osta.assert_not_called()
+
+    def test_lisaa_kortille_rahaa_jos_summa_posi(self):
+        maksukortti_mock = Mock()
+        maksukortti_mock.saldo = 0
+        
+        self.kassa.lataa(maksukortti_mock, 5)
+
+        maksukortti_mock.lataa.assert_called_with(5)
+    
+    def test_ei_lisaa_kortille_rahaa_jos_summa_nega(self):
+        maksukortti_mock = Mock()
+        maksukortti_mock.saldo = 0
+        
+        self.kassa.lataa(maksukortti_mock, -5)
+
+        maksukortti_mock.lataa.assert_not_called()
