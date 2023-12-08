a4fbad32b46f95da7b4b796a9d3e6373233e1e5e
Henni Seppänen <104835482+henniseppis@users.noreply.github.com>
2023-12-08 16:12:02 +0200

    vk6 t.2

---
+++ viikko6/query-language/src/index.py
@@ -0,0 +1,39 @@
+from statistics import Statistics
+from player_reader import PlayerReader
+from matchers import And, HasAtLeast, PlaysIn, HasFewerThan, Not, All
+
+
+def main():
+    url = "https://studies.cs.helsinki.fi/nhlstats/2022-23/players.txt"
+    reader = PlayerReader(url)
+    stats = Statistics(reader)
+
+    matcher = And(
+    HasFewerThan(2, "goals"),
+    PlaysIn("NYR")
+)
+
+    matcher1 = And(
+    Not(HasAtLeast(2, "goals")),
+    PlaysIn("NYR")
+)
+    
+    filtered_with_all = stats.matches(All())
+    print(len(filtered_with_all))
+
+
+
+    matcher = And(c
+        HasAtLeast(5, "goals"),
+        HasAtLeast(20, "assists"),
+        PlaysIn("PHI")
+    )
+    for player in stats.matches(matcher):
+        print(player)
+    
+    for player in stats.matches(matcher1):
+        print(player)
+
+
+if __name__ == "__main__":
+    main()
